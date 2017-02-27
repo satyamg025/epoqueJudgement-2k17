0ffa6807d6d73968eb9156730b1fab6d44c7c473
shreymisra <shreymisra8@gmail.com>
2017-02-27 19:46:50 +0530

    firstcommit

--- app/src/main/java/edu/kiet/www/epoquejudge/MainActivity.java
+++ app/src/main/java/edu/kiet/www/epoquejudge/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Sttring s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
